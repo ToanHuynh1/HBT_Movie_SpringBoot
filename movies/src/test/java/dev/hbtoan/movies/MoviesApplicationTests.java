82ebf0e27e39260fd4ec9f706f574dbcf2a8949f
Toan Bao <baotoandd2016@gmail.com>
2023-06-05 22:14:22 +0700

    end backend

---
+++ movies/src/test/java/dev/hbtoan/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hbtoan.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
